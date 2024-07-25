d51646dea4730111e531435f806f9c874bd2d7ae
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-25 18:26:22 -0300

    Project created

---
+++ src/main/java/com/rm/myadmin/MyadminApplication.java
@@ -0,0 +1,13 @@
+package com.rm.myadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyadminApplication.class, args);
+	}
+
+}
