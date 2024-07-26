bd929e15da58c617880e778c0b6164343f3bbeb1
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-26 19:06:59 -0300

    feat: Service layer, component registration

--- src/main/java/com/rm/myadmin/resources/ContractResource.java
+++ src/main/java/com/rm/myadmin/resources/ContractResource.java
@@ -1,21 +1,32 @@
 package com.rm.myadmin.resources;
 
-import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rm.myadmin.entities.Contract;
+import com.rm.myadmin.services.ContractService;
 
 @RestController
 @RequestMapping(value = "/contracts")
 public class ContractResource {
+	@Autowired
+	private ContractService service;
 
 	@GetMapping
-	public ResponseEntity<Contract> findAll() {
-		Contract c = new Contract(1L, LocalDate.now(), LocalDate.now(), 1500.0, "Andamento");
-		return ResponseEntity.ok().body(c);
+	public ResponseEntity<List<Contract>> findAll() {
+		List<Contract> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Contract> findById(@PathVariable Long id) {
+		Contract obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/rm/myadmin/services/ContractService.java
@@ -0,0 +1,25 @@
+package com.rm.myadmin.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rm.myadmin.entities.Contract;
+import com.rm.myadmin.repositories.ContractRepository;
+
+@Service
+public class ContractService {
+	@Autowired
+	private ContractRepository repository;
+
+	public List<Contract> findAll() {
+		return repository.findAll();
+	}
+
+	public Contract findById(Long id) {
+		Optional<Contract> obj = repository.findById(id);
+		return obj.get();
+	}
+}
