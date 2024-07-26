5a010a3f9e76c53653f1a82b36e092cd8b14a2e5
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-25 21:05:17 -0300

    feat: H2 database, test profile, JPA

--- src/main/java/com/rm/myadmin/entities/Contract.java
+++ src/main/java/com/rm/myadmin/entities/Contract.java
@@ -4,10 +4,21 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_contract")
 public class Contract implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	// private Long residence_id;
 	// private Long tenant_id;
 	private LocalDate contract_start_date;
