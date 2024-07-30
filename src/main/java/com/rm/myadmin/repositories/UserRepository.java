2b9088b247a9a3173ba9e4f07f526d4f31b98baa
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-30 17:51:44 -0300

    refactor: remove UserType enum, UserRepository, UserResource and UserService

--- src/main/java/com/rm/myadmin/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rm.myadmin.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rm.myadmin.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
