c9d8b235b1e08b4cfc180d363af0ea0e91c59bca
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-31 19:19:47 -0300

    feat: inheritance config

--- src/main/java/com/rm/myadmin/entities/User.java
+++ src/main/java/com/rm/myadmin/entities/User.java
@@ -2,12 +2,17 @@ package com.rm.myadmin.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
 public abstract class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
