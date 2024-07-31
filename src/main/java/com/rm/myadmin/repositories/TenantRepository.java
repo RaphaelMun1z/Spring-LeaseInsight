a7515ef64e5bdb14dad40d725b6b5e63394754fc
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-07-31 19:18:24 -0300

    feat: Tenant - repository, resource and service

--- src/main/java/com/rm/myadmin/repositories/TenantRepository.java
+++ src/main/java/com/rm/myadmin/repositories/TenantRepository.java
@@ -1,9 +1,7 @@
 package com.rm.myadmin.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.rm.myadmin.entities.Tenant;
 
-public interface TenantRepository extends JpaRepository<Tenant, Long> {
+public interface TenantRepository extends UserRepository<Tenant> {
 
 }
--- src/main/java/com/rm/myadmin/services/TenantService.java
+++ src/main/java/com/rm/myadmin/services/TenantService.java
@@ -22,4 +22,8 @@ public class TenantService {
 		Optional<Tenant> obj = repository.findById(id);
 		return obj.get();
 	}
+
+	public Tenant create(Tenant obj) {
+		return repository.save(obj);
+	}
 }
