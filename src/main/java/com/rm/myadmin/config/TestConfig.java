5d1b3f8ab2702da7435aaa102ec9b1f5d5991124
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-01 21:28:32 -0300

    test: TestConfig

--- src/main/java/com/rm/myadmin/config/TestConfig.java
+++ src/main/java/com/rm/myadmin/config/TestConfig.java
@@ -69,13 +69,19 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Owner o1 = new Owner(null, "Garen Demácia", "(13) 91234-5678", "garendemacia@gmail.com", "password123");
 		Owner o2 = new Owner(null, "Lux", "(11) 94321-8765", "lux@gmail.com", "senha321");
-
 		ownerRepository.saveAll(Arrays.asList(o1, o2));
+
+		BillingAddress ba1 = new BillingAddress(null, 502, "Av. Pres. Kennedy", "Guilhermina", "Praia Grande",
+				"São Paulo", "Brasil", "33444-111", "Próximo ao Extra");
+		billingAddressRepository.save(ba1);
+
+		Tenant t1 = new Tenant(null, "Nautilus", "(13) 91212-1212", "nautilus@gmail.com", "senhaa123",
+				LocalDate.of(2000, 6, 15), "111.111.111.11", "22.222.222.2", LocalDate.now(), TenantStatus.PENDING,
+				ba1);
+		tenantRepository.save(t1);
 
 		ResidenceAddress ra1 = new ResidenceAddress(null, 315, "Av. Mal. Mallet", "Canto do Forte", "Praia Grande",
 				"São Paulo", "Brasil", "11222-333", "Próximo ao quartel");
-
-		residenceAddressRepository.saveAll(Arrays.asList(ra1));
 
 		Residence r1 = new Residence(null, o1, PropertyType.HOUSE, "Casa nova, moderna, alto padrão.", null,
 				"Condomínio ABC", 3, 2, 1, 112.5f, 87.3f, 2, Year.of(2023), OccupancyStatus.OCCUPIED,
@@ -84,41 +90,27 @@ public class TestConfig implements CommandLineRunner {
 				"Apartamento com vista para o mar, bem iluminado.", 101, "Edifício Solar", 2, 1, 0, 75.0f, 65.0f, 1,
 				Year.of(2022), OccupancyStatus.PENDING_MOVE_OUT, new BigDecimal("850000.00"), new BigDecimal("2000.00"),
 				Instant.parse("2024-06-15T08:30:00Z"), ra1);
-
-		BillingAddress ba1 = new BillingAddress(null, 502, "Av. Pres. Kennedy", "Guilhermina", "Praia Grande",
-				"São Paulo", "Brasil", "33444-111", "Próximo ao Extra");
-
-		billingAddressRepository.save(ba1);
-
-		Tenant t1 = new Tenant(null, "Nautilus", "(13) 91212-1212", "nautilus@gmail.com", "senhaa123",
-				LocalDate.of(2000, 6, 15), "111.111.111.11", "22.222.222.2", LocalDate.now(), TenantStatus.PENDING,
-				ba1);
-
-		tenantRepository.save(t1);
+		residenceRepository.saveAll(Arrays.asList(r1, r2));
+		residenceAddressRepository.save(ra1);
 
 		Contract c1 = new Contract(null, r1, t1, LocalDate.of(2024, 4, 20), LocalDate.of(2024, 10, 10), 950.00,
 				ContractStatus.ACTIVE, 10);
 		Contract c2 = new Contract(null, r2, t1, LocalDate.of(2024, 4, 20), LocalDate.of(2024, 10, 20), 1100.0,
 				ContractStatus.RENEWED, 22);
-
 		contractRepository.saveAll(Arrays.asList(c1, c2));
 
 		r1.setContract(c1);
 		r2.setContract(c2);
 
-		residenceRepository.saveAll(Arrays.asList(r1, r2));
-
 		RentalHistory rh1 = new RentalHistory(null, LocalDate.of(2024, 4, 20), PaymentStatus.PAID, c1);
 		RentalHistory rh2 = new RentalHistory(null, LocalDate.of(2024, 5, 10), PaymentStatus.OVERDUE, c1);
 		RentalHistory rh3 = new RentalHistory(null, LocalDate.of(2024, 6, 10), PaymentStatus.PENDING, c1);
-
 		rentalHistoryRepository.saveAll(Arrays.asList(rh1, rh2, rh3));
 
 		AdditionalFeature af1 = new AdditionalFeature(null, "Piscina");
 		AdditionalFeature af2 = new AdditionalFeature(null, "Churrasqueira");
 		AdditionalFeature af3 = new AdditionalFeature(null, "Placa solar");
 		AdditionalFeature af4 = new AdditionalFeature(null, "Academia");
-
 		additionalFeatureRepository.saveAll(Arrays.asList(af1, af2, af3, af4));
 
 		ResidenceFeature rf1 = new ResidenceFeature(r1, af1);
@@ -126,9 +118,7 @@ public class TestConfig implements CommandLineRunner {
 		ResidenceFeature rf3 = new ResidenceFeature(r1, af3);
 		ResidenceFeature rf4 = new ResidenceFeature(r2, af1);
 		ResidenceFeature rf5 = new ResidenceFeature(r2, af4);
-
 		residenceFeatureRepository.saveAll(Arrays.asList(rf1, rf2, rf3, rf4, rf5));
-
 	}
 
 }
