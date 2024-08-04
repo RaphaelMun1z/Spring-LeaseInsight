fa7dcc809aaabd239753d01bde3f919bba8e1de2
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-04 20:25:36 -0300

    feat: EnableScheduling

--- src/main/java/com/rm/myadmin/MyadminApplication.java
+++ src/main/java/com/rm/myadmin/MyadminApplication.java
@@ -2,12 +2,13 @@ package com.rm.myadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyadminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyadminApplication.class, args);
 	}
-
 }
