d8de5980d100c5f9ea1218723708ff471fc97b5d
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-04 20:30:23 -0300

    test: TestConfig

--- src/main/java/com/rm/myadmin/config/TestConfig.java
+++ src/main/java/com/rm/myadmin/config/TestConfig.java
@@ -15,21 +15,18 @@ import com.rm.myadmin.entities.AdditionalFeature;
 import com.rm.myadmin.entities.BillingAddress;
 import com.rm.myadmin.entities.Contract;
 import com.rm.myadmin.entities.Owner;
-import com.rm.myadmin.entities.RentalHistory;
 import com.rm.myadmin.entities.Residence;
 import com.rm.myadmin.entities.ResidenceAddress;
 import com.rm.myadmin.entities.ResidenceFeature;
 import com.rm.myadmin.entities.Tenant;
 import com.rm.myadmin.entities.enums.ContractStatus;
 import com.rm.myadmin.entities.enums.OccupancyStatus;
-import com.rm.myadmin.entities.enums.PaymentStatus;
 import com.rm.myadmin.entities.enums.PropertyType;
 import com.rm.myadmin.entities.enums.TenantStatus;
 import com.rm.myadmin.repositories.AdditionalFeatureRepository;
 import com.rm.myadmin.repositories.BillingAddressRepository;
 import com.rm.myadmin.repositories.ContractRepository;
 import com.rm.myadmin.repositories.OwnerRepository;
-import com.rm.myadmin.repositories.RentalHistoryRepository;
 import com.rm.myadmin.repositories.ResidenceAddressRepository;
 import com.rm.myadmin.repositories.ResidenceFeatureRepository;
 import com.rm.myadmin.repositories.ResidenceRepository;
@@ -40,9 +37,6 @@ import com.rm.myadmin.repositories.TenantRepository;
 public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private ContractRepository contractRepository;
-
-	@Autowired
-	private RentalHistoryRepository rentalHistoryRepository;
 
 	@Autowired
 	private ResidenceRepository residenceRepository;
@@ -84,7 +78,7 @@ public class TestConfig implements CommandLineRunner {
 		ResidenceAddress ra1 = new ResidenceAddress(null, 315, "Av. Mal. Mallet", "Canto do Forte", "Praia Grande",
 				"São Paulo", "Brasil", "11222-333", "Próximo ao quartel");
 		residenceAddressRepository.save(ra1);
-		
+
 		Residence r1 = new Residence(null, o1, PropertyType.HOUSE, "Casa nova, moderna, alto padrão.", null,
 				"Condomínio ABC", 3, 2, 1, 112.5f, 87.3f, 2, Year.of(2023), OccupancyStatus.OCCUPIED,
 				new BigDecimal("1615900.00"), new BigDecimal("3500.00"), Instant.parse("2024-07-10T12:35:12Z"), ra1);
@@ -95,18 +89,18 @@ public class TestConfig implements CommandLineRunner {
 		residenceRepository.saveAll(Arrays.asList(r1, r2));
 
 		Contract c1 = new Contract(null, r1, t1, LocalDate.of(2024, 4, 20), LocalDate.of(2024, 10, 10), 950.00,
-				ContractStatus.ACTIVE, 10);
+				ContractStatus.ACTIVE, 4);
 		Contract c2 = new Contract(null, r2, t1, LocalDate.of(2024, 4, 20), LocalDate.of(2024, 10, 20), 1100.0,
-				ContractStatus.RENEWED, 22);
+				ContractStatus.RENEWED, 5);
 		contractRepository.saveAll(Arrays.asList(c1, c2));
 
 		r1.setContract(c1);
 		r2.setContract(c2);
 
-		RentalHistory rh1 = new RentalHistory(null, LocalDate.of(2024, 4, 20), PaymentStatus.PAID, c1);
-		RentalHistory rh2 = new RentalHistory(null, LocalDate.of(2024, 5, 10), PaymentStatus.OVERDUE, c1);
-		RentalHistory rh3 = new RentalHistory(null, LocalDate.of(2024, 6, 10), PaymentStatus.PENDING, c1);
-		rentalHistoryRepository.saveAll(Arrays.asList(rh1, rh2, rh3));
+//		RentalHistory rh1 = new RentalHistory(null, LocalDate.of(2024, 4, 20), PaymentStatus.PAID, c1);
+//		RentalHistory rh2 = new RentalHistory(null, LocalDate.of(2024, 5, 10), PaymentStatus.OVERDUE, c1);
+//		RentalHistory rh3 = new RentalHistory(null, LocalDate.of(2024, 6, 10), PaymentStatus.PENDING, c1);
+//		rentalHistoryRepository.saveAll(Arrays.asList(rh1, rh2, rh3));
 
 		AdditionalFeature af1 = new AdditionalFeature(null, "Piscina");
 		AdditionalFeature af2 = new AdditionalFeature(null, "Churrasqueira");
@@ -114,7 +108,7 @@ public class TestConfig implements CommandLineRunner {
 		AdditionalFeature af4 = new AdditionalFeature(null, "Academia");
 		additionalFeatureRepository.saveAll(Arrays.asList(af1, af2, af3, af4));
 
-		//ResidenceFeature rf1 = new ResidenceFeature(r1, af1);
+		// ResidenceFeature rf1 = new ResidenceFeature(r1, af1);
 		ResidenceFeature rf2 = new ResidenceFeature(r1, af2);
 		ResidenceFeature rf3 = new ResidenceFeature(r1, af3);
 		ResidenceFeature rf4 = new ResidenceFeature(r2, af1);
