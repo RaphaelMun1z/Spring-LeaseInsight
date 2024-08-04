49d2b757b3b87f7a149b59c7fcb9657120b3e4d1
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-04 20:37:23 -0300

    feat: findByContractStatus

--- src/main/java/com/rm/myadmin/repositories/ContractRepository.java
+++ src/main/java/com/rm/myadmin/repositories/ContractRepository.java
@@ -1,9 +1,11 @@
 package com.rm.myadmin.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rm.myadmin.entities.Contract;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
-
+	Set<Contract> findByContractStatus(Integer code);
 }
