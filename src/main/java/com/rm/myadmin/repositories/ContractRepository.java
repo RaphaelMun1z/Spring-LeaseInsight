4ba6fce939b1fc90f06933a01d58a5af94150d27
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-05 21:41:45 -0300

    feat: findByTenant

--- src/main/java/com/rm/myadmin/repositories/ContractRepository.java
+++ src/main/java/com/rm/myadmin/repositories/ContractRepository.java
@@ -5,7 +5,10 @@ import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rm.myadmin.entities.Contract;
+import com.rm.myadmin.entities.Tenant;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 	Set<Contract> findByContractStatus(Integer code);
+
+	Set<Contract> findByTenant(Tenant tenant);
 }
