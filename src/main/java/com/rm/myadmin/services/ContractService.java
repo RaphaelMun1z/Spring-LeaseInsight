7e07252cfad5a430fcc95ee41ad3cc0af655e916
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-05 21:38:20 -0300

    feat: send email

--- src/main/java/com/rm/myadmin/services/ContractService.java
+++ src/main/java/com/rm/myadmin/services/ContractService.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Service;
 import com.rm.myadmin.entities.Contract;
 import com.rm.myadmin.entities.RentalHistory;
 import com.rm.myadmin.entities.Residence;
+import com.rm.myadmin.entities.Tenant;
+import com.rm.myadmin.entities.enums.ContractStatus;
 import com.rm.myadmin.entities.enums.PaymentStatus;
 import com.rm.myadmin.repositories.ContractRepository;
 import com.rm.myadmin.services.exceptions.DatabaseException;
@@ -35,6 +37,9 @@ public class ContractService {
 
 	@Autowired
 	private RentalHistoryService rentalHistoryService;
+
+	@Autowired
+	private EmailService emailService;
 
 	public List<Contract> findAll() {
 		return repository.findAll();
@@ -96,22 +101,45 @@ public class ContractService {
 		return repository.findByContractStatus(code);
 	}
 
-	@Scheduled(cron = "0 0 0 * * *")
+	public Set<Contract> findByTenant(Long id) {
+		Tenant tenant = tenantService.findById(id);
+		return repository.findByTenant(tenant);
+	}
+
+	@Scheduled(cron = "0 50 20 * * *")
 	public void checkContractInvoiceDueDate() {
 		LocalDate today = LocalDate.now();
 		Set<Contract> contracts = this.findByContractStatus(1);
 
 		for (Contract c : contracts) {
-			int dueDate = c.getInvoiceDueDate();
-			if ((int) dueDate > today.getMonth().length(Year.isLeap(today.getYear()))) {
-				dueDate = today.getMonth().length(Year.isLeap(today.getYear()));
-			}
+			if (c.getContractEndDate().equals(today)) {
+				c.setContractStatus(ContractStatus.TERMINATED);
+			} else {
+				int dueDate = c.getInvoiceDueDate();
+				if ((int) dueDate > today.getMonth().length(Year.isLeap(today.getYear()))) {
+					dueDate = today.getMonth().length(Year.isLeap(today.getYear()));
+				}
 
-			if (dueDate == today.getDayOfMonth()) {
-				System.out.println("O contrato com ID " + c.getId() + " está vencido hoje.");
-				System.out.println("Gerar fatura.");
-				RentalHistory rental = new RentalHistory(null, today, PaymentStatus.PENDING, c);
-				rentalHistoryService.create(rental);
+				if (dueDate == today.getDayOfMonth()) {
+					System.out.println("O contrato com ID " + c.getId() + " está vencido hoje.");
+					System.out.println("Gerar fatura.");
+					RentalHistory rental = new RentalHistory(null, today, PaymentStatus.PENDING, c);
+					rentalHistoryService.create(rental);
+					String emailBody = "Olá " + c.getTenant().getName() + ",\n\n" + "Esperamos que você esteja bem!\n\n"
+							+ "Gostaríamos de informar que a sua fatura de aluguel referente ao período de "
+							+ rental.getRentalStartDate() + " - " + rental.getRentalEndDate()
+							+ " já está disponível para consulta.\n\n" + "Detalhes da Fatura:\n" + "- Valor: R$ "
+							+ rental.getRentalValue() + "\n" + "- Data de Vencimento: " + rental.getRentalEndDate()
+							+ "\n\n"
+							+ "Para acessar a fatura e realizar o pagamento, por favor, acesse o nosso portal através do link abaixo:\n"
+							+ "www.myadmin.com\n\n"
+							+ "Se tiver qualquer dúvida ou precisar de assistência, não hesite em entrar em contato conosco.\n\n"
+							+ "Agradecemos pela sua atenção.\n\n" + "Atenciosamente,\n" + "MyAdmin Locações\n" + "---\n"
+							+ "Este é um e-mail automático, por favor, não responda.";
+
+					System.out.println(emailService.sendEmail(c.getTenant().getEmail(),
+							"[Fatura Disponível] Sua Fatura de Aluguel Já Está Disponível", emailBody));
+				}
 			}
 		}
 		System.out.println("verificações do dia finalizadas.");
