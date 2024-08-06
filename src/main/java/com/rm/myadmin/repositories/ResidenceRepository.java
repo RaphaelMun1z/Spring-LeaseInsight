e43051160c1f2b18c093d68d624ea909ee29602f
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-08-05 21:42:18 -0300

    feat: findByOwner

--- src/main/java/com/rm/myadmin/repositories/ResidenceRepository.java
+++ src/main/java/com/rm/myadmin/repositories/ResidenceRepository.java
@@ -1,9 +1,12 @@
 package com.rm.myadmin.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rm.myadmin.entities.Owner;
 import com.rm.myadmin.entities.Residence;
 
 public interface ResidenceRepository extends JpaRepository<Residence, Long> {
-
+	Set<Residence> findByOwner(Owner owner);
 }
